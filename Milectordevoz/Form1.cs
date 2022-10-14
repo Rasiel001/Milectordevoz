d7fb8adf2efa04d4db704c586505003b5ae73bab
Alumno <Alumno@PC-18>
2022-10-14 12:14:55 -0500

    Agregar archivos de proyecto.

---
+++ Milectordevoz/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Speech;
+using System.Speech.Synthesis;
+
+namespace Milectordevoz
+{
+    public partial class Form1 : Form
+    {
+        SpeechSynthesizer reader = new SpeechSynthesizer();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            reader.SpeakAsync(label1.Text);//leemo el contenido del label
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Stream str;
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if(openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                if((str=openFileDialog.OpenFile())!=null)
+                {
+                    string fname = openFileDialog.FileName;
+                    string filetxt = File.ReadAllText(fname);
+                    label1.Text = filetxt;
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(reader != null)
+            {
+                reader.Dispose();
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (reader != null)
+            {
+                if(reader.State == SynthesizerState.Speaking)
+                {
+                    reader.Pause();
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (reader != null)
+            {
+                if (reader.State == SynthesizerState.Paused)
+                {
+                    reader.Resume();
+                }
+            }
+        }
+    }
+}
